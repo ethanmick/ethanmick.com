48f40f7bbf23d15dfef596b6acc72503ae55aeec
Ethan Mick <ethan@ethanmick.com>
2019-12-02 16:45:05 -0500

    Add database schema

---
+++ db/index.sql
@@ -0,0 +1,60 @@
+CREATE EXTENSION pgcrypto;
+CREATE EXTENSION pg_stat_statements;
+
+begin transaction;
+
+create table links (
+  id serial not null primary key,
+  link text not null,
+  title text,
+  author text,
+  date_published timestamp without time zone,
+  lead_image_url text,
+  domain text,
+  read boolean default false,
+  -- Model Objects
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone
+);
+
+-- Also Magic Decks?
+create table magic_ranks (
+  id serial not null primary key,
+  rank text not null,
+  -- Model Objects
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone
+);
+
+create table stars (
+  id integer not null,
+  name text not null,
+  owner text not null,
+  full_name text not null,
+  description text,
+  url text not null,
+  language text,
+  forks integer,
+  stargazers_count integer,
+  last_updated_at timestamp without time zone,
+  -- Model Objects
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone
+);
+
+create table tweets (
+  -- Tweet Properties
+  id text not null primary key,
+  text text not null,
+  -- User Properties 
+  user_name text not null,
+  user_screen_name text not null,
+  user_profile_image_url text not null,
+  entities jsonb,
+  -- Model Objects
+  created_at timestamp without time zone not null default now(),
+  updated_at timestamp without time zone
+);
+
+
+commit;
