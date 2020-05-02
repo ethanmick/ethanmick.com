14e072293b0b04741df260f081ab7f0e42ef742d
Ethan Mick <ethan@ethanmick.com>
2020-05-01 21:00:25 -0400

    Update dependencies & cleanup (#13)
    Add theme to code blocks, but I need to set the language on most of them
    for this to work.
    
    * Remove `@zeit/next-css`. Use built in CSS support.
    * Add blockquote support css

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
-const withCSS = require('@zeit/next-css')
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/
 })
 
-module.exports = withCSS(
-  withMDX({
-    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-    publicRuntimeConfig: {
-      API_ROOT: process.env.API_ROOT,
-      ROOT_URL: process.env.ROOT_URL || 'https://ethanmick.com'
-    },
-    devIndicators: {
-      autoPrerender: false
-    }
-  })
-)
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  publicRuntimeConfig: {
+    API_ROOT: process.env.API_ROOT,
+    ROOT_URL: process.env.ROOT_URL || 'https://ethanmick.com'
+  },
+  devIndicators: {
+    autoPrerender: false
+  }
+})
--- server/github.ts
+++ server/github.ts
@@ -1,4 +1,4 @@
-import Octokit from '@octokit/rest'
+import { Octokit } from '@octokit/rest'
 import moment from 'moment'
 import { github } from './config'
 import { Star } from './models'
