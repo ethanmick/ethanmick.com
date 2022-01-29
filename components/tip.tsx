fdeeff304ea2eebe23802ed3390d3cbaa3ec9c69
Ethan Mick <ethan@ethanmick.com>
2022-01-29 16:23:19 -0500

    Add Tip component and update language (#36)

---
+++ components/tip.tsx
@@ -0,0 +1,12 @@
+import { LightBulbIcon } from '@heroicons/react/outline'
+
+type Props = {
+  children: React.ReactNode
+}
+
+export const Tip = ({ children }: Props) => (
+  <div className="flex items-center rounded-lg bg-sky-200 bg-opacity-40 p-2">
+    <LightBulbIcon className="h-12 w-12 p-2 text-rose-500" />
+    <span>{children}</span>
+  </div>
+)
