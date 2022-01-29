45feb3ac2ea5a73baca684f0438690a94674296f
Ethan Mick <ethan@ethanmick.com>
2022-01-29 14:10:55 -0500

    Setup Fathom Analytics (#34)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
+import * as Fathom from 'fathom-client'
 import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import React from 'react'
+import { Router } from 'next/router'
+import React, { useEffect } from 'react'
 import '../styles/globals.css'
 
+Router.events.on('routeChangeComplete', () => {
+  Fathom.trackPageview()
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Fathom.load('JVRRQIEZ', {
+      includedDomains: ['*.ethanmick.com'],
+      url: 'https://reassuring-champ.ethanmick.com/script.js',
+    })
+  }, [])
   return (
     <>
       <Head>
