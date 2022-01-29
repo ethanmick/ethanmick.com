cdfa12f77aa3d46fb6d76b952e752aab3ea51aa9
Ethan Mick <ethan@ethanmick.com>
2022-01-29 11:27:35 -0500

    Fix build, add linting (#30)

--- components/title.tsx
+++ components/title.tsx
@@ -8,7 +8,7 @@ export const Title = ({ className }: TitleProps) => (
   <div className={className ? className : 'py-20'}>
     <Link href="/">
       <a className="hover:underline">
-        <h1 className="text-4xl text-center">Ethan Mick</h1>
+        <h1 className="text-center text-4xl">Ethan Mick</h1>
       </a>
     </Link>
   </div>
