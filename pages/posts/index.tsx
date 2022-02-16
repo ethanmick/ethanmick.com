8f5a928f69c171449945dc575faf538dcea5cbcb
Ethan Mick <ethan@ethanmick.com>
2022-02-16 16:40:52 -0500

    Add React guide to posts (#51)

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -10,6 +10,13 @@ export default function PostPage({ metas }: Props) {
   return (
     <Layout>
       <h1 className="py-8 text-center text-4xl">Posts</h1>
+      <h2 className="py-8 text-2xl">React</h2>
+      <Link href="/react/introduction">
+        <a className="block py-2 text-blue-500 visited:text-indigo-700 hover:underline">
+          Introduction to React
+        </a>
+      </Link>
+      <h2 className="py-8 text-2xl">Other</h2>
       {metas.map(({ slug, title }) => (
         <Link href={`/posts/${slug}`} key={slug}>
           <a className="block py-2 text-blue-500 visited:text-indigo-700 hover:underline">
