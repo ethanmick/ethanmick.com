38d23fd97eb8b09cb932dd83f4721046a2f1789d
Ethan Mick <ethan@ethanmick.com>
2022-02-25 18:11:39 -0500

    New header look and feel (#58)

--- components/header.tsx
+++ components/header.tsx
@@ -19,17 +19,22 @@ const navigation = [
 ]
 
 export const Header = () => (
-  <header className="py-2 shadow">
+  <header className="py-4">
     <div className="container mx-auto flex items-center justify-between px-4">
-      <Link href={Routes.Home}>
-        <a className="text-2xl">Ethan Mick</a>
-      </Link>
+      <div className="flex items-center md:divide-x md:leading-10">
+        <Link href={Routes.Home}>
+          <a className="pr-6 text-2xl">Ethan Mick</a>
+        </Link>
+        <span className="hidden pl-6 font-light text-slate-600 md:inline-block">
+          Learn, build, ship.
+        </span>
+      </div>
       <nav>
         {navigation.map((link) => (
           <Link href={link.href} key={link.name}>
             <a
               className={cx(
-                'ml-8 text-base font-medium text-black hover:text-gray-800 hover:underline',
+                'ml-8 text-base font-medium text-black hover:text-slate-800 hover:underline',
                 link.className
               )}
             >
