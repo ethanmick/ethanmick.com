d8d4c5f25b047d9d01d7786e32251bcf65fa30ec
Ethan Mick <ethan@ethanmick.com>
2022-02-27 21:04:10 -0500

    Line numbers (#60)

--- components/mdx/code.tsx
+++ components/mdx/code.tsx
@@ -16,13 +16,38 @@ export const Code = ({ children, className }: any) => {
           className={cx('rounded', className)}
           style={{ ...style, padding: '20px' }}
         >
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
+          <div className="relative">
+            <div className="line-numbers pointer-events-none absolute left-0 block w-8">
+              {tokens.map((_, i) => (
+                <span
+                  key={i}
+                  className="block text-right text-slate-200 text-opacity-50"
+                />
               ))}
             </div>
-          ))}
+            <code className="relative block pl-14">
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </code>
+          </div>
+          <style jsx>{`
+            .line-numbers > span {
+              counter-increment: linenumber;
+            }
+
+            .line-numbers > span:before {
+              content: counter(linenumber);
+              font-size: 14.2222px;
+              display: block;
+              text-align: right;
+              user-select: none;
+            }
+          `}</style>
         </pre>
       )}
     </Highlight>
