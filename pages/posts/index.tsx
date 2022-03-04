ed30a4c2dd005b9f61fec7c23ed724de68b2196e
Ethan Mick <ethan@ethanmick.com>
2022-03-04 09:11:03 -0800

    React components guide section

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -22,6 +22,11 @@ export default function PostPage({ metas }: Props) {
           Getting Started with React &amp; TypeScript
         </a>
       </Link>
+      <Link href={Routes.ReactComponents}>
+        <a className="block py-2 text-blue-500 visited:text-indigo-700 hover:underline">
+          Components
+        </a>
+      </Link>
       <h2 className="py-8 text-2xl">Other</h2>
       {metas.map(({ slug, title }) => (
         <Link href={`/posts/${slug}`} key={slug}>
